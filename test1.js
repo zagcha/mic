7e8d6ed4a0d4c639c5a053a074770f610a1f0e99
zagcha <64097681+zagcha@users.noreply.github.com>
2021-04-05 03:07:38 +0300

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,4 @@
+if(location.protocol!='https:'){location.protocol='https:';}
+$(`<link rel="stylesheet" href="https://cdn.statically.io/gh/Zagchatt/you/main/kaz.css">
+<style>
+</style>`).insertBefore('body');

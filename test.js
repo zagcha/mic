944a21d9edf03da23b75c6371fb421eca92616ae
zagcha <64097681+zagcha@users.noreply.github.com>
2021-04-05 03:00:47 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+if(location.protocol!='https:'){location.protocol='https:';}
+$(`<link rel="stylesheet" href="https://cdn.statically.io/gh/Zagchatt/you/main/kaz.css">
+$(`<link rel="stylesheet" href="https://mulukiraq.com/css/app.css">
+<style>
+</style>`).insertBefore('body');
+$.getScript("https://mulukiraq.com/js/app.js");
